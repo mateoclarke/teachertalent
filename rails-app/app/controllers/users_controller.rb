8c5e5c72964007e4958d2bc6d7a72d06d74a452b
mateoclarke <code@mateoclarke.com>
2015-02-07 21:48:05 -0600

    Fix bug on sign out

--- rails-app/app/controllers/users_controller.rb
+++ rails-app/app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-before_filter :authenticate_user!
+# before_filter :authenticate_user!
 
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
+    @user = current_user
 
     respond_to do |format|
         format.html # show.html.erb
