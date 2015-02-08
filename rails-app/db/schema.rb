ffa63b832eb12d0405acc7f3944bf7537a358e61
mateoclarke <code@mateoclarke.com>
2015-02-07 20:32:00 -0600

    scaffold up teacher model

--- rails-app/db/schema.rb
+++ rails-app/db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150208014553) do
+ActiveRecord::Schema.define(version: 20150208022629) do
+
+  create_table "schools", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "city"
+    t.text     "state"
+    t.text     "district"
+    t.string   "picture"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
